9fd7da495f6ff2e8384e882ac8826809852f6df4
werserk <werserk@inbox.ru>
2022-07-14 13:23:23 +0300

    metrics changes

--- modeling/metrics.py
+++ modeling/metrics.py
@@ -1,32 +1,28 @@
-class IoUScore(object):
-    def __init__(self, smooth=1):
-        self.smooth = smooth
+import torch
 
-    def __call__(self, inputs, targets):
-        assert inputs.shape == targets.shape, f"{inputs.shape} - {targets.shape}"
-        inputs = inputs.view(-1)
-        targets = targets.view(-1)
 
-        # intersection is equivalent to True Positive count
-        # union is the mutually inclusive area of all labels & predictions
-        intersection = (inputs * targets).sum()
-        total = (inputs + targets).sum()
+class IoUScore:
+    def __init__(self, eps=1e-7):
+        super().__init__()
+        self.eps = eps
+
+    def __call__(self, y_pred, y_true):
+        assert y_pred.shape == y_true.shape, str(y_pred.shape) + ' != ' + str(y_true.shape)
+        intersection = torch.sum(y_true * y_pred, dim=[2, 3])
+        total = torch.sum(y_true, dim=[2, 3]) + torch.sum(y_pred, dim=[2, 3])
         union = total - intersection
+        iou = ((intersection + self.eps) / (union + self.eps)).mean(dim=1).mean(dim=0)
+        return 1 - iou
 
-        IoU = (intersection + self.smooth) / (union + self.smooth)
-        return IoU
 
+class DiceScore:
+    def __init__(self, eps=1e-7):
+        super().__init__()
+        self.eps = eps
 
-class DiceLoss(object):
-    def __init__(self, smooth):
-        self.smooth = smooth
-
-    def forward(self, inputs, targets):
-        # flatten label and prediction tensors
-        inputs = inputs.view(-1)
-        targets = targets.view(-1)
-
-        intersection = (inputs * targets).sum()
-        dice = (2. * intersection + self.smooth) / (inputs.sum() + targets.sum() + self.smooth)
-
+    def __call__(self, y_pred, y_true):
+        assert y_pred.shape == y_true.shape, str(y_pred.shape) + ' != ' + str(y_true.shape)
+        intersection = torch.sum(y_true * y_pred, dim=[2, 3])
+        total = torch.sum(y_true, dim=[2, 3]) + torch.sum(y_pred, dim=[2, 3])
+        dice = ((intersection + self.eps) / (total + self.eps)).mean(dim=1).mean(dim=0)
         return 1 - dice
